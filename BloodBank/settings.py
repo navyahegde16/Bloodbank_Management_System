db946341fa32b2fa41e2b43fcb1cdf518b7140c0
navyahegde16 <navyahegde16@gmail.com>
2021-05-19 12:10:24 +0530

    0.2

--- BloodBank/settings.py
+++ BloodBank/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iw8)hzniv-@68f)p)4s6cxgwoi%j)m$y6!+sqah%ew(*^%t+wh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bloodbank-ms.herokuapp.com','127.0.0.1']
 
 
 # Application definition
